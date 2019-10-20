9a78fc125a648a052811b7cfb00d1aff96827a5c
Joshua Rosenthal <jrosen52@binghamton.edu>
2019-10-19 22:46:50 -0400

    Math game code

--- app/src/main/java/com/example/menugame/ui/game/DashboardViewModel.java
+++ app/src/main/java/com/example/menugame/ui/game/DashboardViewModel.java
@@ -3,10 +3,64 @@ package com.example.menugame.ui.game;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import java.util.Random;
 
 public class DashboardViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
+
+    public void addProb()
+    {
+        Random rand = new Random();
+        int op1 = rand.nextInt(21 - (-20)) + (-20);
+        int op2 = rand.nextInt(21 - (-20)) + (-20);
+        int sol = op1 + op2;
+    }
+
+    public void subProb()
+    {
+        Random rand = new Random();
+        int op1 = rand.nextInt(21 - (-20)) + (-20);
+        int op2 = rand.nextInt(21 - (-20)) + (-20);
+        int sol = op1 - op2;
+
+    }
+
+    public void multProb()
+    {
+        Random rand = new Random();
+        int op1 = rand.nextInt(16 - (-15)) + (-15);
+        int op2 = rand.nextInt(16 - (-15)) + (-15);
+        int sol = op1 * op2;
+    }
+
+    public void divProb()
+    {
+
+    }
+
+    public void mathGame()
+    {
+        Random rand = new Random();
+        int probType = rand.nextInt(4);
+        if(probType == 0)
+        {
+            addProb();
+        }
+        else if(probType == 1)
+        {
+            subProb();
+        }
+        else if(probType == 2)
+        {
+            multProb();
+        }
+        else
+        {
+            divProb();
+        }
+
+    }
 
     public DashboardViewModel() {
         mText = new MutableLiveData<>();
